e3fe10b920e9860ec1a4d8d8e7ec4c665bc7ee4b
MukitHossen7 <hossenmukit7@gmail.com>
2025-07-29 16:45:05 +0600

    use transction rollback

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -7,38 +7,50 @@ import config from "../../config";
 import { Wallet } from "../wallet/wallet.model";
 
 const createUser = async (payload: Partial<IUser>) => {
-  const { email, password, ...rest } = payload;
-  const isExistUser = await User.findOne({ email });
+  const session = await User.startSession();
+  session.startTransaction();
+  try {
+    const { email, password, ...rest } = payload;
+    const isExistUser = await User.findOne({ email });
 
-  if (isExistUser) {
-    throw new AppError(httpStatus.BAD_REQUEST, "User Already Exist");
-  }
+    if (isExistUser) {
+      throw new AppError(httpStatus.BAD_REQUEST, "User Already Exist");
+    }
 
-  const hashPassword = await bcrypt.hash(
-    password as string,
-    Number(config.BCRYPT_SALT_ROUNDS)
-  );
-  const user = await User.create({
-    email,
-    password: hashPassword,
-    ...rest,
-  });
-
-  const existingWallet = await Wallet.findOne({
-    user: user._id,
-  });
-  if (existingWallet) {
-    throw new AppError(
-      httpStatus.CONFLICT,
-      "Wallet already exists for this user"
+    const hashPassword = await bcrypt.hash(
+      password as string,
+      Number(config.BCRYPT_SALT_ROUNDS)
     );
-  }
+    const user = await User.create({
+      email,
+      password: hashPassword,
+      ...rest,
+    });
 
-  await Wallet.create({
-    user: user._id,
-    balance: 50,
-  });
-  return user;
+    const existingWallet = await Wallet.findOne({
+      user: user._id,
+    });
+    if (existingWallet) {
+      throw new AppError(
+        httpStatus.CONFLICT,
+        "Wallet already exists for this user"
+      );
+    }
+
+    await Wallet.create({
+      user: user._id,
+      balance: 50,
+    });
+
+    await session.commitTransaction();
+    session.endSession();
+
+    return user;
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    throw error;
+  }
 };
 
 export const UserServices = {
