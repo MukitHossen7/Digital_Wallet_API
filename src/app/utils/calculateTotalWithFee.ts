7dc2c5ea746407375b781db532d38fdb3079cee2
MukitHossen7 <hossenmukit7@gmail.com>
2025-08-26 01:13:46 +0600

    some update

--- src/app/utils/calculateTotalWithFee.ts
+++ src/app/utils/calculateTotalWithFee.ts
@@ -1,7 +1,4 @@
-export const calculateTotalWithFee = (
-  amount: number,
-  feePerThousand = 18.5
-) => {
+export const calculateTotalWithFee = (amount: number, feePerThousand = 10) => {
   const feeRate = feePerThousand / 1000;
   const fee = parseFloat((amount * feeRate).toFixed(2));
   const totalAmount = parseFloat((amount + fee).toFixed(2));
